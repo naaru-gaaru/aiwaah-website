76043c2ddc512d348944717b6a21f23ac0c1449f
naaru-gaaru <narayan.govindaraj@gmail.com>
2026-01-03 21:27:46 -0500

    Supabase const redeclaration

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ const input = document.getElementById("user-input");
 const SUPABASE_URL = "https://rqwnwkmjeiyagvyzdumi.supabase.co";
 const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJxd253a21qZWl5YWd2eXpkdW1pIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjczOTI4NzMsImV4cCI6MjA4Mjk2ODg3M30.T7h97fh3jgUC7NVa9O3Nz024ZWuR6Wz4d_fx7MKE5mo";
 
-const supabase = supabaseJs.createClient(
+const supabaseClient = supabaseJs.createClient(
   SUPABASE_URL,
   SUPABASE_ANON_KEY
 );
@@ -12,7 +12,7 @@ const googleBtn = document.getElementById("login-google");
 
 if (googleBtn) {
   googleBtn.addEventListener("click", async () => {
-    await supabase.auth.signInWithOAuth({
+    await supabaseClient.auth.signInWithOAuth({
       provider: "google"
     });
   });
@@ -100,6 +100,7 @@ form.addEventListener("submit", async (e) => {
     setInputDisabled(false);
   }
 });
+
 
 
 
